69039aa0aaf243abab565a653d15e495d0c4c1a8
AmedamaSpica <spica96amedama@gmail.com>
2023-10-24 15:33:35 +0900

    わっしょいの処理を作りました。

--- src/Assets/Scripts/Washoi_script.cs
+++ src/Assets/Scripts/Washoi_script.cs
@@ -6,26 +6,70 @@ public class Washoi_script : MonoBehaviour
 {
 
     float Input_washoi;
+    float old_washoi;
+    float Input_washoi_once;
+    [SerializeField] float wasyoi_hankei = 5.0f;
+
+    MikoshiCollisionDetection MCD;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        MCD = MCD.GetComponent<MikoshiCollisionDetection>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Input_washoi = Input.GetAxis("Fire1");
+        Input_washoi_once = old_washoi - Input_washoi;
 
-        if(Input_washoi < 0)
+        old_washoi = Input_washoi;
+
+        if (Input_washoi_once == 1)
         {
-            
-            GameObject.Find("People");
+            Check_People();
         }
+        
+    }
+    private void FixedUpdate()
+    {
+        
 
-        Debug.Log(Input_washoi);
+        
     }
 
+    void Check_People()
+    {
+
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, wasyoi_hankei,Vector3.forward);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.tag == "People")
+            {
+                Debug.Log($"検出されたオブジェクト {hit.collider.name}");
+
+                MCD.peopleCount++;
+                Destroy(hit.collider.gameObject);
+
+                Debug.Log(MCD.peopleCount);
+
+                //人の生成
+                MCD.GenerateMikoshiPeople();
+
+                if (MCD.peopleCount >= MCD.clearConditions && MCD.isFever == false)
+                {
+                    MCD.isFever = true;
+                    MCD.FeverTime();
+                }
+
+
+            }
+        }
+    }
 
 }
+
+
+
