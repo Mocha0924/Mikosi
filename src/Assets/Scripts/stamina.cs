3f501003c4272dd53c04ab2f284259d4e5b2f833
AmedamaSpica <spica96amedama@gmail.com>
2023-10-31 15:12:43 +0900

    スタミナの処理を修正しました

--- src/Assets/Scripts/stamina.cs
+++ src/Assets/Scripts/stamina.cs
@@ -7,8 +7,8 @@ public class stamina : MonoBehaviour
 
     const int stamina_number_first = 3;
     int[] stamina_value;
-    int stamina_number_now = 2;
-    int stamina_heal_sec = 5;
+    int stamina_number_now = stamina_number_first - 1;
+    int stamina_heal_needTime = 5;
     int time = 0;
 
     Washoi_script washoi_Script;
@@ -26,7 +26,7 @@ public class stamina : MonoBehaviour
         
         foreach(int stamina in stamina_value)
         {
-            stamina_value[stamina] = stamina_heal_sec;
+            stamina_value[stamina] = 0;
         }
     }
 
@@ -35,34 +35,44 @@ public class stamina : MonoBehaviour
     {
         if(player.Input_Jump_once == 1)
         {
-            stamina_value[stamina_number_now] -= stamina_heal_sec;
+            stamina_value[stamina_number_now] = 0;
+
             if (stamina_number_now != 0) { stamina_number_now--; }
         }
 
         if (washoi_Script.Input_washoi_once == 1)
         {
-            stamina_value[stamina_number_now] -= stamina_heal_sec;
+            stamina_value[stamina_number_now] -= stamina_heal_needTime;
 
             if (stamina_number_now != 0) { stamina_number_now--; }
 
 
         }
 
-        Debug.Log(stamina_number_now);
 
+        Debug.Log(stamina_value[stamina_number_now] + "=" + stamina_number_now) ;
 
     }
-
-
 
     private void FixedUpdate()
     {
         time++;
 
-        if(time >= 200 )
+        if(time >= 50 )
         {
+            
             stamina_value[stamina_number_now] += 1;
-            if (stamina_number_now < 2) { stamina_number_now++; }
+
+            if (stamina_value[stamina_number_now] == stamina_heal_needTime)
+            {              
+                if (stamina_number_now < 2)
+                {
+                    stamina_value[stamina_number_now] = 0;
+                    stamina_number_now++;
+                   
+                }
+
+            }
 
             time = 0;
         }
