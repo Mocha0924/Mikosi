a0ac84a7db46c19b5c5e555b8baac1855a69e574
Mocha0924 <108125975+Mocha0924@users.noreply.github.com>
2023-10-31 17:16:12 +0900

    コースを4種類追加しました

--- src/Assets/Scripts/LoadDirector.cs
+++ src/Assets/Scripts/LoadDirector.cs
@@ -12,12 +12,26 @@ public class LoadDirector : MonoBehaviour
     [SerializeField] private GameObject Load;
     public GameObject StartTurn;
     public int turnCount = 0;
+    public int MaxTurnCount;
     public bool turnCheck = false;
     public bool RightTurn = false;
-   
+    [SerializeField] private List<GameObject> Patarn_List;
+    public List<GameObject> Cource_List;
+    public int CourseCount = 0;
+
+    private void Start()
+    {
+        CourceDecision();
+    }
     public void LoadPlus(GameObject Load)
     {
         Load_List.Add(Load);
+        CourseCount++;
+        if(CourseCount >= Cource_List.Count)
+        {
+            CourceDecision();
+            CourseCount = 0;
+        }
     }
     public void LoadReduce()
     {
@@ -41,6 +55,19 @@ public class LoadDirector : MonoBehaviour
     {
         StartTurn = start;
     }
-  
-   
+
+    private void CourceDecision()
+    {
+        Cource_List = Patarn_List;
+        for (int i = Cource_List.Count - 1; i > 0; i--)
+        {
+            var j = Random.Range(0, i + 1);
+            var temp = Cource_List[i];
+            Cource_List[i] = Cource_List[j];
+            Cource_List[j] = temp;
+        }
+    }
+
+
+
 }
