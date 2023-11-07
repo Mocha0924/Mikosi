2348d539d3532855bb2ffb95314ef759ba74c7d2
ebifry22 <g2227011@st.t-kougei.ac.jp>
2023-11-07 15:33:59 +0900

    AfterpeopleMoveScript追加

---
+++ src/Assets/Scripts/AfterPeopleMoveScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterPeopleMoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 destination)
+    {
+        Debug.Log("Move:" + destination);
+    }
+}
