c74cb5ffb05579e88d14c1a54cc99352a83799e6
Mocha0924 <108125975+Mocha0924@users.noreply.github.com>
2023-12-05 19:29:14 +0900

    Revert "Merge branch 'main' of https://github.com/Mocha0924/Mikosi"
    This reverts commit af352b0afb3b7e61ac7ecd91405bc5a87034c916, reversing
    changes made to 7e94bdfbf908dddf0255de27a91f81125e179029.

--- src/Assets/Scripts/stamina.cs
+++ src/Assets/Scripts/stamina.cs
@@ -59,7 +59,7 @@ public class stamina : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(MCD.playerMode == MikoshiCollisionDetection.PlayerMode.Gameover || MCD.playerMode == MikoshiCollisionDetection.PlayerMode.Clear)
+        if(MCD.playerMode == MikoshiCollisionDetection.PlayerMode.Gameover)
   
             foreach (int stamina in stamina_value)
             {
