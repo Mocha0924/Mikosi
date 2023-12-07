b00036f2c200285bd95cad85e122baee3433d1e1
Mocha0924 <108125975+Mocha0924@users.noreply.github.com>
2023-12-07 16:19:34 +0900

    集めた人のアニメーションを適応させました

--- src/Assets/Scripts/AfterPeopleAnimationController.cs
+++ src/Assets/Scripts/AfterPeopleAnimationController.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class AfterPeopleAnimationController : MonoBehaviour
 {
     private Animator animator;
+    private MikoshiCollisionDetection mikosiCollision;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         animator.Play(animator.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, Random.Range(0f, 1f));
+        mikosiCollision = GameObject.Find("Player").GetComponent<MikoshiCollisionDetection>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int Colum = mikosiCollision.ColumnCount;
+        animator.SetFloat("Speed", 2 + (Colum * 0.2f + 1));
     }
 }
