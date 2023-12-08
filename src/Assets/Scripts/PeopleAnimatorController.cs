cc93be764ebd6b58807a11e749ec23fcf7cb7add
mocha_show <fuka.sho0224@gmail.com>
2023-12-09 00:11:09 +0900

    一般人のアニメーションを適応させました

---
+++ src/Assets/Scripts/PeopleAnimatorController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleAnimatorController : MonoBehaviour
+{
+    private Animator PeopleAnimator;
+    [SerializeField] private bool Rand;
+    [SerializeField] private int AnimationNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PeopleAnimator = GetComponent<Animator>();
+        if(Rand)
+        {
+            int Randnum = UnityEngine.Random.Range(1,4);
+            PeopleAnimator.SetBool("Case"+Randnum,true);
+        }
+        else if(AnimationNum > 0&&AnimationNum<4)
+            PeopleAnimator.SetBool("Case" +AnimationNum, true);
+    }
+
+   
+}
