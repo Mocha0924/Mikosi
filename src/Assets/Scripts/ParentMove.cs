33f913f31a2cc23688c38ef2e0219a5c1d427e94
ebifry22 <g2227011@st.t-kougei.ac.jp>
2023-12-09 15:55:18 +0900

    ParentMove追加

--- src/Assets/Scripts/ParentMove.cs
+++ src/Assets/Scripts/ParentMove.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class ParentMove : MonoBehaviour
 {
     GameObject parent;
-    int childCount;
-
+    float childCount;
+    GameObject target;
+    Vector3 offset;
+    Vector3 pos;
+    player p;
+    
     // Start is called before the first frame update
     void Start()
     {
-        childCount = this.gameObject.transform.parent.childCount - 1;
-        Debug.Log("AfterPeople:" + childCount);
+        //target = GameObject.Find("Player");
+        //childCount = this.gameObject.transform.parent.childCount - 1;
+        //Debug.Log("AfterPeople:" + childCount);
+        //Vector3 offset = new Vector3(0f, 0.25f, -0.7f - 0.6f * childCount);
+        //Debug.Log(offset);
+        p = GetComponent<player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //pos = target.GetComponent<Vector3>();
+        //Invoke(nameof(Move), 1);
+    }
+
+    void Move()
+    {
+        //transform.position = pos + offset;
     }
 }
