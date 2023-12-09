79eaa43cb23a49254751a243fc6f1c05e7567b05
ebifry22 <g2227011@st.t-kougei.ac.jp>
2023-12-09 20:11:03 +0900

    後ろの人のジャンプ追加

--- src/Assets/Scripts/ParentMove.cs
+++ src/Assets/Scripts/ParentMove.cs
@@ -6,31 +6,97 @@ public class ParentMove : MonoBehaviour
 {
     GameObject parent;
     float childCount;
-    GameObject target;
-    Vector3 offset;
+    Rigidbody rigidbody;
+    Vector3 firstPos;
     Vector3 pos;
     player p;
-    
+
+    bool isJump;
+    bool isRightSride;
+    bool isLeftSride;
+    Vector3 velocty;
+
+    float Input_Horizontal;
+    float old_Horizontal;
+    float Input_Vertical;
+    float old_Vertical;
+    float Input_Jump;
+    float old_Jump;
+
     // Start is called before the first frame update
     void Start()
     {
-        //target = GameObject.Find("Player");
-        //childCount = this.gameObject.transform.parent.childCount - 1;
-        //Debug.Log("AfterPeople:" + childCount);
-        //Vector3 offset = new Vector3(0f, 0.25f, -0.7f - 0.6f * childCount);
-        //Debug.Log(offset);
+        childCount = this.gameObject.transform.parent.childCount - 1;
+        Debug.Log("AfterPeople:" + childCount);
+        rigidbody = this.GetComponent<Rigidbody>();
         p = GetComponent<player>();
+
+        isJump = false;
+        isRightSride = false;
+        isLeftSride = false;
+        velocty = new Vector3(0, 10f, 0);
+        pos = Vector3.zero;
+        firstPos = this.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //pos = target.GetComponent<Vector3>();
-        //Invoke(nameof(Move), 1);
+        Input_Horizontal = Input.GetAxis("Horizontal");
+        Input_Vertical = Input.GetAxis("Vertical");
+        Input_Jump = Input.GetAxis("Jump");
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            isJump = true;
+            Invoke("Jump", childCount);
+        }
+
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    isRightSride = true;
+        //    Invoke("RSride", childCount);
+        //}
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    isLeftSride = true;
+        //    Invoke("LSride", childCount);
+        //}
+
+        if (isRightSride == false && isLeftSride == false)
+        {
+            pos = this.transform.localPosition;
+            pos.x = 0;
+            this.transform.localPosition = pos;
+        }
+
+        pos = this.transform.localPosition;
+        pos.z = firstPos.z;
+        this.transform.localPosition = pos;
+
     }
 
-    void Move()
+    void Jump()
     {
-        //transform.position = pos + offset;
+        Debug.Log("Jump" + childCount);
+
+        rigidbody.velocity = velocty;
+
+        isJump = false;
+    }
+
+    void RSride()
+    {
+        Debug.Log("RSride" + childCount);
+    }
+
+    void LSride()
+    {
+        Debug.Log("LSride" + childCount);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isJump= false;
     }
 }
