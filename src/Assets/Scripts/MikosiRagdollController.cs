c6652ddb008a460de4ef8f35332e513cd9786576
mocha_show <fuka.sho0224@gmail.com>
2023-12-11 00:50:36 +0900

    ゲームオーバー演出を完成させました

---
+++ src/Assets/Scripts/MikosiRagdollController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MikosiRagdollController : MonoBehaviour
+{
+    private Rigidbody[] rBody;
+    private BoxCollider[] BoxCol;
+    private MeshCollider[] MeshCol;
+    private Animator animator;
+    void Start()
+    {
+        
+        animator = GetComponent<Animator>();
+        rBody = GetComponentsInChildren<Rigidbody>();
+        BoxCol = GetComponentsInChildren<BoxCollider>();
+        MeshCol = GetComponentsInChildren<MeshCollider>();
+        foreach (Rigidbody RB in rBody)
+        {
+            RB.isKinematic = true;
+        }
+        foreach (BoxCollider BC in BoxCol)
+        {
+            BC.enabled = false;
+        }
+        foreach(MeshCollider MC in MeshCol)
+        {
+            MC.enabled = false;
+        }
+    }
+
+
+    public void SetRagDoll()
+    {
+        animator.enabled = false;
+       
+        foreach (Rigidbody RB in rBody)
+        {
+            RB.isKinematic = false;
+        }
+        foreach (BoxCollider BC in BoxCol)
+        {
+            BC.enabled = true;
+        }
+        foreach (MeshCollider MC in MeshCol)
+        {
+            MC.enabled = true;
+        }
+    }
+}
