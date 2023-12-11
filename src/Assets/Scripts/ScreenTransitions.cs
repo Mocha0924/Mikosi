b8b07b06e37844b5583632dab07042ea99fa8340
AmedamaSpica <spica96amedama@gmail.com>
2023-12-11 21:26:02 +0900

    タイトルの追加と各種画像の追加

---
+++ src/Assets/Scripts/ScreenTransitions.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+public class ScreenTransitions : MonoBehaviour
+{
+
+
+    [SerializeField] private Image ScreenImages;
+    [SerializeField] private Sprite[] ScreenSprits;
+    [SerializeField] private GameObject[] BeforeObj;
+    [SerializeField] private GameObject[] AfterObj;
+
+    int Sprites_Maxnum;
+    int Sprites_Nownum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprites_Nownum = 0;
+        Sprites_Maxnum = ScreenSprits.Length;
+        ScreenImages.sprite = ScreenSprits[Sprites_Nownum];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Menutrantitons()
+    {
+
+        foreach (var Before in BeforeObj)
+        {
+            Before.SetActive(false);
+        }
+
+        foreach(var After in AfterObj)
+        {
+            After.SetActive(true);
+        }
+
+    }
+
+    public void Screentransitions()
+    {
+
+        ScreenImages.sprite = ScreenSprits[0];
+        
+    }
+
+}
