6b21144b03b671fa545da6f2be4aad77319d224c
Mocha0924 <108125975+Mocha0924@users.noreply.github.com>
2023-12-12 10:06:02 +0900

    音量変更できるスライダーの追加

---
+++ src/Assets/Scripts/AudioVolumeChange.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolumeChange : MonoBehaviour
+{
+    [SerializeField] private AudioSource[] audioSources;
+    private void Start()
+    {
+        foreach(AudioSource source in audioSources)
+        {
+            if(source != null)
+                source.volume = VolumeController.volume;
+        }
+    }
+}
