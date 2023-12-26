104a8784f9c38977696c23352339c57bb4537c38
mocha_show <fuka.sho0224@gmail.com>
2023-12-26 18:34:30 +0900

    取った人が移動している方向を向くようにしました

--- src/Assets/Scripts/AfterPeopleMoveScript.cs
+++ src/Assets/Scripts/AfterPeopleMoveScript.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,8 @@ using UnityEngine;
 public class AfterPeopleMoveScript : MonoBehaviour
 {
     [SerializeField] Vector3 point;
-    [SerializeField] Vector3 goPoint;
 
+   // [SerializeField] Vector3 goPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,24 +17,40 @@ public class AfterPeopleMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        Vector3 diff = point - transform.localPosition;
+        if (diff.magnitude > 1f)
+        {
+            transform.localRotation = Quaternion.LookRotation(diff); 
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Slerp(this.transform.localRotation, Quaternion.Euler(0, 0, 0), 10*Time.deltaTime);
+        }
+        // Move();
     }
 
     public void Move()
     {
         //this.transform.localPosition = point;
         transform.localPosition = Vector3.MoveTowards(transform.localPosition,point, 10.0f*Time.deltaTime);
+        transform.DOLocalMove(point, 10);
     }
 
     public void Setpoint(Vector3 setPos)
     {
         Debug.Log(this.name + "setPos:" + setPos);
         point = setPos;
-       
+        transform.DOLocalMove(point, 1);
+
     }
 
     public Vector3 GetPoint()
     {
         return point;
+    }
+
+    public void FoodDeath()
+    {
+
     }
 }
